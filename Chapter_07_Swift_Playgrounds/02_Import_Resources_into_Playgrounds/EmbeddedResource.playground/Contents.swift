4c72f1b378c4c512951761de5a3eac1d17302022
Keith Moon <keef@keefmoon.com>
2020-07-08 00:44:41 +0100

    Updated code for Chapter 7 Recipe 2

--- Chapter_07_Swift_Playgrounds/02_Import_Resources_into_Playgrounds/EmbeddedResource.playground/Contents.swift
+++ Chapter_07_Swift_Playgrounds/02_Import_Resources_into_Playgrounds/EmbeddedResource.playground/Contents.swift
@@ -1,8 +1,9 @@
-//: Playground - noun: a place where people can play
-
+//: # Bar Chart - iOS
 import PlaygroundSupport
 import UIKit
 
+//: ## Color
+//: Color as a struct can produces UIColor on demand
 struct Color {
     let red: Float
     let green: Float
@@ -17,11 +18,19 @@ struct Color {
     }
 }
 
+/*:
+ ## Bar
+ Simple struct to hold details aboout a bar in a bar chart
+ */
 struct Bar {
     var value: Float
     var color: Color
 }
 
+/*:
+ ## BarView
+ View that that will display the value of a bar in a chart
+ */
 class BarView: UIView {
     
     init(frame: CGRect, color: UIColor) {
@@ -39,13 +48,26 @@ class BarView: UIView {
     private func setupTexture() {
         guard let textureImage = UIImage(named: "texture") else { return }
         let textureColor = UIColor(patternImage: textureImage)
-        let textureView = UIView(frame: CGRect(origin: .zero, size: bounds.size))
+        let frame = CGRect(origin: .zero, size: bounds.size)
+        let textureView = UIView(frame: frame)
         textureView.backgroundColor = textureColor
         addSubview(textureView)
     }
 }
 
+/*:
+ ## BarChart
+ View that that will display at bar chart
+ 
+ ### How to use
+ Add `Bar`s to the `bars` array to add them to the chart
+ */
 class BarChart: UIView {
+    
+    private var barViews: [BarView] = []
+    private var maxValue: Float = 0.0
+    
+    var interBarMargin: CGFloat = 5.0
     
     var bars: [Bar] = [] {
         didSet {
@@ -53,7 +75,7 @@ class BarChart: UIView {
             
             var barViews = [BarView]()
             
-            let barCount: Int = bars.count
+            let barCount = CGFloat(bars.count)
             
             // Calculate the max value before calculating size
             for bar in bars {
@@ -62,11 +84,18 @@ class BarChart: UIView {
             
             var xOrigin: CGFloat = interBarMargin
             
+            let margins = interBarMargin * (barCount+1)
+            let width = (frame.width - margins) / barCount
+            
             for bar in bars {
-                let width = (frame.size.width - (interBarMargin * CGFloat(barCount+1))) / CGFloat(barCount)
+                
                 let height = barHeight(forValue: bar.value)
-                let rect = CGRect(x: xOrigin, y: bounds.height - height, width: width, height: height)
-                let view = BarView(frame: rect, color: bar.color.displayColor)
+                let rect = CGRect(x: xOrigin,
+                                  y: bounds.height - height,
+                                  width: width,
+                                  height: height)
+                let view = BarView(frame: rect,
+                                   color: bar.color.displayColor)
                 barViews.append(view)
                 addSubview(view)
                 
@@ -75,16 +104,19 @@ class BarChart: UIView {
             self.barViews = barViews
         }
     }
-    var interBarMargin: CGFloat = 5.0
-    
-    private var barViews: [BarView] = []
-    private var maxValue: Float = 0.0
     
     private func barHeight(forValue value: Float) -> CGFloat {
         return (frame.size.height / CGFloat(maxValue)) * CGFloat(value)
     }
 }
 
+/*:
+ ## Usage
+ 
+ * Create Bar Chart
+ * Create Bars and add to chart
+ * Make Bar Chart the LiveView
+ */
 let barView = BarChart(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
 barView.backgroundColor = .white
 
