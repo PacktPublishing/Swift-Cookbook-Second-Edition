1359e3b91fb6cd011602955590c82c6b5cdd8493
Chris Barker <chrisbbarker@gmail.com>
2021-02-23 18:05:05 +0000

    no message

--- Chapter08/TaskAPI/Package.swift
+++ Chapter08/TaskAPI/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0"),
         .package(url: "https://github.com/Kitura/BlueSocket.git", from: "1.0.0"),
-        .package(path: "/Users/chris/Source/Swift-5-Cookbook-Second-Edition/Chapter_08_Server_Side_Swift/TaskModule")
+        //.package(path: "/Users/Reader/Location-to-your/TaskModule")
     ],
     targets: [
         .target(
--- Chapter08/TaskAPI/Sources/App/configure.swift
+++ Chapter08/TaskAPI/Sources/App/configure.swift
@@ -9,13 +9,22 @@ public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    app.databases.use(.postgres(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
-    ), as: .psql)
+    if let databaseURL = Environment.get("DATABASE_URL"),
+       var postgresConfig = PostgresConfiguration(url: databaseURL) {
+        postgresConfig.tlsConfiguration = .forClient(certificateVerification: .none)
+        app.databases.use(.postgres(configuration: postgresConfig),as: .psql)
+        
+    } else {
+        
+        app.databases.use(.postgres(
+            hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+            port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
+            username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+            password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+            database: Environment.get("DATABASE_NAME") ?? "vapor_database"
+        ), as: .psql)
+        
+    }
 
     //app.migrations.add(CreateTodo())
     app.migrations.add(CreateTask())
